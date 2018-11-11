f2cb329c90bc944dc09d925c028be67978791856
ericlian <ericlian@umich.edu>
2018-11-10 20:00:43 -0500

    Updated models.py

--- models/Models.py
+++ models/Models.py
@@ -15,23 +15,48 @@ import pandas as pd
 
 
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
-data = pd.DataFrame.from_csv("2010-17data.csv").values
-year = 2017
+YEAR = 2017
+NUM_GAMES = 1230
+NUM_GAMES_11_12 = 990
 
 def getTrainData(year):
+    '''
+    Parameters:
+        year: season of interest
+    Returns:
+        two arrays, X and Y, representing training features and labels
+    '''
+    data = pd.DataFrame.from_csv("2010-17data.csv").values
     l = len(data)
+    
+    #Getting three years of training data prior to the year of interest
     if(year > 2014):
-        X = data[l - 1230 * (2021-year):l - 1230 * (2018-year), 3:]
-        Y = data[l - 1230 * (2021-year):l - 1230 * (2018-year), 0]
+        X = data[l - NUM_GAMES * (2021-year):l - NUM_GAMES * (2018-year), 3:]
+        Y = data[l - NUM_GAMES * (2021-year):l - NUM_GAMES * (2018-year), 0]
     else:
-        X = data[l- 1230 * (2020-year)-990:l- 1230 * (2017-year), 3:]
-        Y = data[l - 1230 * (2020-year)-990:l - 1230 * (2017-year), 0]
+        X = data[l- NUM_GAMES * (2020-year)-NUM_GAMES_11_12:l- NUM_GAMES * (2017-year), 3:]
+        Y = data[l - NUM_GAMES * (2020-year)-NUM_GAMES_11_12:l - NUM_GAMES * (2017-year), 0]
 
     return X,Y
 
-def create_model(year=2017, optimizer = 'adam', activation = 'relu', neuron_config  = [64,64], 
+def train_and_eval_model(year=2017, optimizer = 'adam', activation = 'relu', neuron_config  = [64,64], 
                  epochs = 75, batch_size = 10, dropout_config = [0.7,0.7], 
                  weight_constraint = None, initializer = 'glorot_uniform', verbose = 2):
+    '''
+    Parameters:
+        year: season of interest
+        optimizer: optimizer of choice
+        activation: layer activation of choice
+        neuron_config: an array representing # of neurons in each layer
+        epochs: # of epochs to train
+        batch_size: batch size
+        dropout_config: an array representing % of dropout in each dropout layer
+        weight_constraint: the weight for neurons
+        initializer: initializer of choice that initializes the weights
+        verbose: verbose level
+    Returns:
+        A trained keras model
+    '''
     from keras.constraints import max_norm
     # Load the datasets
     X,Y = getTrainData(year)
@@ -60,27 +85,39 @@ def create_model(year=2017, optimizer = 'adam', activation = 'relu', neuron_conf
     
     return model
 
-#Grid Search Codes (might or might not delete in the future)
-model = KerasClassifier(build_fn=create_model)
-X,Y = getTrainData(year)
- 
-# activations = ['softmax','elu','selu','softplus','softsign','relu','tanh','sigmoid','hard_sigmoid',
-#                'linear']
-dropout_configs = np.array([[[a/10, b/10] for b in range(10)] for a in range(10)])
-dropout_configs.resize(1,100)
-print(dropout_configs)
-weight_constraints = np.arange(6)
-initializers = ['RandomNormal','RandomUniform',"TruncatedNormal",'VarianceScaling',
-                'Orthogonal','lecun_uniform','glorot_normal','glorot_uniform','he_normal','lecun_normal',
-                'he_uniform']
-param_grid = dict(dropout_config = dropout_configs, weight_constraint = weight_constraints)
-grid = GridSearchCV(estimator=model, param_grid=param_grid)
-grid_result = grid.fit(X,Y)
- 
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+def gridSearch():
+    #Grid Search Codes to find best config (to be updated)#
+    ##############################################################
+    model = KerasClassifier(build_fn=create_model)
+    X,Y = getTrainData(year)
+     
+    # activations = ['softmax','elu','selu','softplus','softsign','relu','tanh','sigmoid','hard_sigmoid',
+    #                'linear']
+    dropout_configs = np.array([[[a/10, b/10] for b in range(10)] for a in range(10)])
+    dropout_configs.resize(1,100)
+    print(dropout_configs)
+    weight_constraints = np.arange(6)
+    initializers = ['RandomNormal','RandomUniform',"TruncatedNormal",'VarianceScaling',
+                    'Orthogonal','lecun_uniform','glorot_normal','glorot_uniform','he_normal','lecun_normal',
+                    'he_uniform']
+    param_grid = dict(dropout_config = dropout_configs, weight_constraint = weight_constraints)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid)
+    grid_result = grid.fit(X,Y)
+     
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+    ##############################################################
+
+#Main function that train and evaluate and save the model
+def main():
+    model = train_and_eval_model(year = YEAR)
+    model.save(str(YEAR) + ".h5")
+
+#Allows for directly running main() from terminal with "python Models.py"
+if __name__ == '__main__':
+    main()
