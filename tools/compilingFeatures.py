4cf546a94e9fe96b6eceedbd847d725475f8125d
km2018 <karthik.murthy123@gmail.com>
2018-12-02 00:11:16 -0600

    Clean first seven methods of compilingfeatures.py

--- tools/compilingFeatures.py
+++ tools/compilingFeatures.py
@@ -6,6 +6,7 @@ from requests import get
 import numpy as np
 import pandas as pd
 
+def findStat(teamID, data):
 '''
     Parameters:
         teamID: the ID for the team of interest
@@ -15,7 +16,6 @@ import pandas as pd
     Returns:
         a numpy array with the stats for the specific team from the season data csv
 '''
-def findStat(teamID, data):
     
     indices = (data.index.values).astype(np.int64)
     for r in range(0, len(indices)):
@@ -23,6 +23,7 @@ def findStat(teamID, data):
             return np.array((data.iloc[r, :]))
             break
 
+def getGames(season):
 '''
     Parameters:
         season: the season for the games
@@ -30,48 +31,60 @@ def findStat(teamID, data):
     Returns:
         a pandas Dataframe containing the games for specified season
 '''
-def getGames(season):
-    
-    compilation = []
-    indices = []
+    TEAM_ID_COLUMN = 1
+    GAME_MATCHUP_COLUMN = 6
+    GAME_RESULT_COLUMN = 7
+    DATAFRAME_COLUMNS = ['B_W/L', 'C_hostID', 'D_visitingID']
 
-    season = season
-    binaryWL = {'W': 1, 'L': 0}
+    season_gamelog = []
+    game_numbers = []
 
-    games = league.GameLog(season=season, player_or_team='T')
+    winloss_to_binary = {'W': 1, 'L': 0}
 
-    scores = games.overall()
+    gamelog_object = league.GameLog(season = season, player_or_team = 'T')
+    gamelog = gamelog_object.overall()
+    sorted_games = gamelog.sort_values(by = ['GAME_ID'], ascending = True)
 
-    finScores = scores.sort_values(by=['GAME_ID'], ascending=True)
+    for i in range(len(sorted_games)):
+        
+        game_data = []
 
-    for i in range(len(finScores)):
-        temp = []
-        if (i % 2 == 1 or i == len(finScores) - 1):
+        # Skip every other entry
+        # Every 2 entries represents a game so this must be done
+        if (i % 2 == 1 or i == len(sorted_games) - 1):
             continue
-        matchup = str(finScores.iloc[i, 6])
+
+        matchup = str(sorted_games.iloc[i, GAME_MATCHUP_COLUMN])
+
+        # If vs. is in matchup sorted_games[i] is the home team
         if "vs." not in matchup:
-            WL = binaryWL[finScores.iloc[i + 1, 7]]
-            temp.append(WL)
-            hostID = int(finScores.iloc[i + 1, 1])
-            roadID = int(finScores.iloc[i, 1])
-            temp.append(hostID)
-            temp.append(roadID)
+            binary_result = winloss_to_binary[sorted_games.iloc[i + 1, GAME_RESULT_COLUMN]]
+            game_data.append(binary_result)
+
+            home_id = int(sorted_games.iloc[i + 1, GAME_RESULT_COLUMN])
+            road_id = int(sorted_games.iloc[i, GAME_RESULT_COLUMN])
+
+            game_data.append(home_id)
+            game_data.append(road_id)
+        
+        # If vs. is not in matchup sorted_games[i] is the road team
         else:
-            key = finScores.iloc[i, 7]
-            WL = binaryWL[key]
-            temp.append(WL)
-            hostID1 = int(finScores.iloc[i, 1])
-            roadID1 = int(finScores.iloc[i + 1, 1])
-            temp.append(hostID1)
-            temp.append(roadID1)
-        compilation.append(temp)
-        indices.append(int((i / 2) + 1))
+            binary_result = winloss_to_binary[sorted_games.iloc[i, GAME_RESULT_COLUMN]]
+            game_data.append(binary_result)
 
-    columns = ['B_W/L', 'C_hostID', 'D_visitingID']
+            home_id = int(sorted_games.iloc[i, GAME_RESULT_COLUMN])
+            road_id = int(sorted_games.iloc[i + 1, GAME_RESULT_COLUMN])
 
-    kaboom = pd.DataFrame(data=compilation, index=indices, columns=columns)
-    return kaboom
+            game_data.append(home_id)
+            game_data.append(road_id)
 
+        season_gamelog.append(game_data)
+        game_numbers.append(int((i / 2) + 1))
+
+    season_gamelog_dataframe = pd.DataFrame(data = season_gamelog, index = game_numbers, columns = DATAFRAME_COLUMNS)
+    return season_gamelog_dataframe
+
+def getTeamPIE(year, min_games_played = 45, min_mins_played = 25, num_players_considered = 2):
 '''
     Parameters:
         year: the season/year of interest
@@ -84,51 +97,63 @@ def getGames(season):
             qualified as the top players on the teams to contribute the Team PIE.
             Default is 25 minutes.
 
+        num_players_considered: by default, the function gets the two players with the highest PIE
+            for each team, but the number of players considered can be changed by changing
+            num_players
+
     Returns:
-        a pandas DataFrame containing the Player Impact Estimate (PIE)
-    of the two highest ranked players for each team
+        a pandas DataFrame containing the total Player Impact Estimate (PIE)
+        of the two highest ranked players for each team
 '''
-def getTeamPIE(year,min_games_played = 45, mins_played = 25):
+    DESIRED_COLUMNS = ['PLAYER_ID', 'PLAYER_NAME', 'TEAM_ID', 'PIE']
 
-    obj = league.PlayerStats(measure_type="Advanced", season=year)
-    df = obj.overall()
-    PIE = df.sort_values(by=['PIE'], ascending=False)
-    nonQualis = []
-    teamIDs = []
-    teamPIE = []
+    advanced_player_stats = league.PlayerStats(measure_type = "Advanced", season = year)
+    all_advanced_stats = advanced_player_stats.overall()
+    sorted_stats = all_advanced_stats.sort_values(by = ['PIE'], ascending = False)
+    
+    unqualified_players = []
+    team_ids = []
+    team_pies = []
 
-    for i in range(0, len(PIE)):
-        if(PIE['GP'][i] <= min_games_played and PIE['MIN'][i] <= mins_played):
-            nonQualis.append(i)
-        else:
-            pass
-    PIE = PIE.drop(nonQualis)
-    PIE = PIE[['PLAYER_ID', 'PLAYER_NAME', 'TEAM_ID', 'PIE']]
+    for current_player in range(len(sorted_stats)):
+        if(sorted_stats['GP'][current_player] <= min_games_played and sorted_stats['MIN'][current_player] <= mins_played):
+            unqualified_players.append(current_player)
+
+    sorted_stats = sorted_stats.drop(unqualified_players)
+    sorted_stats = sorted_stats[DESIRED_COLUMNS]
+
     for team in constants.TEAMS:
-        teamIDs.append(constants.TEAMS[team]['id'])
-    for team in teamIDs:
-        temp = 0
-        onePIE = 0
-        for index, row in PIE.iterrows():
-            if(temp == 2):
-                teamPIE.append(onePIE)
-                break
-            if(int(row['TEAM_ID']) == int(team)):
-                onePIE += row['PIE']
-                temp += 1
-    aggregate = pd.DataFrame(data=teamPIE, index=teamIDs, columns=['teamPIE'])
-    return(aggregate)
+        team_ids.append(constants.TEAMS[team]['id'])
 
+    for team_id in team_ids:
+        counter = 0
+        team_pie = 0
+
+        for index, row in sorted_stats.iterrows():
+
+            if(counter == num_players_considered):
+                team_pies.append(team_pie)
+                break
+
+            if(int(row['TEAM_ID']) == int(team_id)):
+                team_pie += row['PIE']
+                counter += 1
+
+    team_pie_dataframe = pd.DataFrame(data = team_pies, index = team_ids, columns = ['Team PIE'])
+    return(team_pie_dataframe)
+
+def getDF(season, measure_type):
 '''
     Parameters:
         season: the season of interest
-        measureType: 'Four Factors' or 'Advanced'
+        measure_type: 'Four Factors' or 'Advanced'
     Returns:
         a pandas DataFrame containing all teams' specified type of stats
 '''
-def getDF(season, measureType):
-    
-    url = "https://stats.nba.com/stats/leaguedashteamstats"
+    FOUR_FACTORS_END_COLUMN = 15
+    ADVANCED_END_COLUMN = 9
+
+    URL = "https://stats.nba.com/stats/leaguedashteamstats"
     HEADERS = {
         'user-agent': ('Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'),  # noqa: E501
         'Dnt': ('1'),
@@ -136,9 +161,9 @@ def getDF(season, measureType):
         'Accept-Language': ('en'),
         'origin': ('http://stats.nba.com')
         }
-    headers = dict(HEADERS)
+    DICT_HEADERS = dict(HEADERS)
 
-    params = {
+    PARAMS = {
         'Conference': '',
         'DateFrom': '',
         'DateTo': '',
@@ -170,42 +195,39 @@ def getDF(season, measureType):
         'VsDivision': ''
     }
 
-    # 0, 6:
-    # Index = teamIDs
-
-    _get = get(url, params=params, headers=headers)
+    _get = get(URL, params = PARAMS, headers = DICT_HEADERS)
     data = _get.json()
-    headers1 = data['resultSets'][0]['headers']
-    rows1 = data['resultSets'][0]['rowSet']
+    headers = data['resultSets'][0]['headers']
+    rows = data['resultSets'][0]['rowSet']
 
     indices = []
-    data = []
-    end = 0
+    season_data = []
+    end_column = 0
 
-    if(measureType == "Four Factors"):
-        end = 15
-    elif(measureType == "Advanced"):
-        end = 9
+    if(measure_type == "Four Factors"):
+        end_column = FOUR_FACTORS_END_COLUMN
+    elif(measure_type == "Advanced"):
+        end_column = ADVANCED_END_COLUMN
 
-    for row in rows1:
+    for row in rows:
         indices.append(row[0])
-        data.append(row[7:end])
+        season_data.append(row[7:end_column])
 
-    total = np.array(data)
-    df = pd.DataFrame(data=total, columns=headers1[7:end], index=indices)
-    return (df)
+    np_season_data = np.array(season_data)
+    season_data_dataframe = pd.DataFrame(data = np_season_data, columns = headers[7:end_column], index = indices)
+    return (season_data_dataframe)
 
+def getTeamLocationDF(team_id, season):
 '''
     Parameters:
-        teamID: ID for the team of intersst
+        team_id: ID for the team of intersst
         season: season of interest
     Returns:
         a pandas DataFrame containing the team's four factors
         based on the location of game (home or away)
 '''
-def getTeamLocationDF(teamID, season):
 
-    url = "https://stats.nba.com/stats/teamdashboardbygeneralsplits"
+    URL = "https://stats.nba.com/stats/teamdashboardbygeneralsplits"
     HEADERS = {
         'user-agent': ('Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'),  # noqa: E501
         'Dnt': ('1'),
@@ -213,9 +235,9 @@ def getTeamLocationDF(teamID, season):
         'Accept-Language': ('en'),
         'origin': ('http://stats.nba.com')
         }
-    headers = dict(HEADERS)
+    DICT_HEADERS = dict(HEADERS)
 
-    params = {
+    PARAMS = {
         'Conference': '',
         'DateFrom': '',
         'DateTo': '',
@@ -242,37 +264,40 @@ def getTeamLocationDF(teamID, season):
         'SeasonType': "Regular Season",
         'ShotClockRange': '',
         'StarterBench': '',
-        'TeamID': teamID,
+        'TeamID': team_id,
         'VsConference': '',
         'VsDivision': ''
     }
 
-    _get = get(url, params=params, headers=headers)
-    data = _get.json()
-    headers1 = data['resultSets'][1]['headers']
-    values = data['resultSets'][1]['rowSet']
+    _get = get(url, params = PARAMS, headers = DICT_HEADERS)
+    season_data = _get.json()
+    headers = season_data['resultSets'][1]['headers']
+    values = season_data['resultSets'][1]['rowSet']
 
-    return (pd.DataFrame(data=values, columns=headers1))
+    return (pd.DataFrame(data = values, columns = headers))
 
+def getTeamCourt(team_id, season, court_type):
 '''
     Parameters:
-        teamID: Id of the team of interest
+        team_id: Id of the team of interest
         season: season of interest
-        courtType: 'home' or 'road'
+        court_type: 'home' or 'road'
     Returns:
         an array containing the team's court data
 '''
-def getTeamCourt(teamID, season, courtType):
     
-    court = getTeamLocationDF(teamID=teamID, season=season)
+    court = getTeamLocationDF(team_id = team_id, season = season)
     if(courtType == "home"):
         i = 0
+
     elif(courtType == "road"):
         i = 1
+
     return [court['EFG_PCT'][i], court['FTA_RATE'][i], court['TM_TOV_PCT'][i],
             court['OREB_PCT'][i], court['OPP_EFG_PCT'][i], court["OPP_FTA_RATE"][i],
             court['OPP_TOV_PCT'][i], court['OPP_OREB_PCT'][i]]
 
+def getCourtData(season):
 '''
     Requires:
         teamData.csv under current working directory
@@ -281,30 +306,32 @@ def getTeamCourt(teamID, season, courtType):
     Returns:
         a pandas DataFrame containing all teams' court data for the season
 '''
-def getCourtData(season):
-    
-    teamData = pd.DataFrame.from_csv("teamData.csv")
-    teamIds = teamData.index.values
 
-    blah = []
-    c = ["Home_EFG_PCT", "Home_FTA_RATE", "Home_TM_TOV_PCT", "Home_OREB_PCT",
+    METRICS = ["Home_EFG_PCT", "Home_FTA_RATE", "Home_TM_TOV_PCT", "Home_OREB_PCT",
     "Home_OPP_EFG_PCT","Home_OPP_FTA_RATE","Home_OPP_TOV_PCT","Home_OPP_OREB_PCT",
     "Road_EFG_PCT","Road_FTA_RATE","Road_TM_TOV_PCT","Road_OREB_PCT","Road_OPP_EFG_PCT",
-    "Road_OPP_FTA_RATE","Road_OPP_TOV_PCT",	"Road_OPP_OREB_PCT"]
+    "Road_OPP_FTA_RATE","Road_OPP_TOV_PCT", "Road_OPP_OREB_PCT"]
+    TIME_BETWEEN_FUNCTION_CALLS = 2
 
-    for i in range(len(teamIds)):
-        temp = []
-        home = getTeamCourt(teamID=teamIds[i], season=season, courtType="home")
-        time.sleep(2)
-        road = getTeamCourt(teamID=teamIds[i], season=season, courtType="road")
+    team_data = pd.DataFrame.from_csv("teamData.csv")
+    team_ids = teamData.index.values
 
-        [temp.append(home[i]) for i in range(len(home))]
-        [temp.append(road[i]) for i in range(len(road))]
-        blah.append(temp)
+    court_data = []
 
-    csv = pd.DataFrame(data=blah, columns=c, index=teamIds)
-    return csv
+    for i in range(len(team_ids)):
+        team_court_data = []
+        home_court_data = getTeamCourt(team_id = team_ids[i], season = season, courtType = "home")
+        time.sleep(TIME_BETWEEN_FUNCTION_CALLS)
+        road_court_data = getTeamCourt(team_id = team_ids[i], season = season, courtType = "road")
 
+        [team_court_data.append(home_court_data[i]) for i in range(len(home_court_data))]
+        [team_court_data.append(road_court_data[i]) for i in range(len(road_court_data))]
+        court_data.append(team_court_data)
+
+    court_data_dataframe = pd.DataFrame(data = court_data, columns = METRICS, index = team_ids)
+    return court_data_dataframe
+
+def getSeasonDF(season):
 '''
     Requires:
         2010-17data.csv under the current working directory
@@ -314,7 +341,6 @@ def getCourtData(season):
         a pandas DataFrame containing all team stats of current
         interest for the given season
 '''
-def getSeasonDF(season):
     
     gameLogs = getGames(season)
     ff = getDF(season, "Four Factors")
@@ -369,6 +395,8 @@ def getSeasonDF(season):
     bam = pd.DataFrame(data=values, columns=column)
     return bam
 
+# basically nothing to do
+def getSeasonsDF(startYear, endYear):
 '''
     Parameters:
         startYear: year to start at (inclusive)
@@ -377,8 +405,7 @@ def getSeasonDF(season):
         a pandas DataFrame containing all team stats from
         startYear to endYear
 '''
-def getSeasonsDF(startYear, endYear):
-    
+
     seasonsDF = pd.concat([getSeasonDF(str(i) + "-" + str((i + 1))[2:])
                            for i in range(startYear, endYear)])
     return seasonsDF
